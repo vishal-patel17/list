520246709a511fde22c9683b6a89f9dfad160dd4
Patel <vp185104@ncr.com>
2018-12-10 17:00:09 +0530

    Created App Layout

--- lib/main.dart
+++ lib/main.dart
@@ -11,26 +11,76 @@ class MyApp extends StatelessWidget {
         appBar: AppBar(
           title: Text("Lists"),
         ),
+        floatingActionButton: FloatingActionButton(
+          onPressed: () {},
+          child: Icon(Icons.chat),
+        ),
         body: ListView(
           children: <Widget>[
+            SizedBox(
+              height: 20.0,
+            ),
             Padding(
               padding: const EdgeInsets.all(8.0),
-              child: new GestureDetector(
-                onTap: (){
-                  debugPrint("Clicked!");
-                },
-                child: Card(
-                  child: Column(
-                    mainAxisSize: MainAxisSize.min,
-                    children: <Widget>[
-                      const ListTile(
-                        leading: Icon(Icons.shopping_cart),
-                        title: Text("Shopping list"),
-                        subtitle:
-                            Text("Add/Remove items from the shopping list"),
-                      ),
-                    ],
-                  ),
+              child: Card(
+                color: Colors.blue,
+                child: Column(
+                  mainAxisSize: MainAxisSize.min,
+                  children: <Widget>[
+                    ListTile(
+                      onTap: () {
+                        //debugPrint("Clicked!");
+                      },
+                      leading: Icon(Icons.shopping_cart),
+                      title: Text("Shopping list"),
+                      subtitle: Text("Add/Remove items from the shopping list"),
+                    ),
+                  ],
+                ),
+              ),
+            ),
+            SizedBox(
+              height: 20.0,
+            ),
+            Padding(
+              padding: const EdgeInsets.all(8.0),
+              child: Card(
+                color: Colors.red,
+                child: Column(
+                  mainAxisSize: MainAxisSize.min,
+                  children: <Widget>[
+                    ListTile(
+                      onTap: () {
+                        //debugPrint("Clicked!");
+                      },
+                      leading: Icon(Icons.today),
+                      title: Text("Task list"),
+                      subtitle: Text("Add/Remove your daily tasks"),
+                    ),
+                  ],
+                ),
+              ),
+            ),
+            SizedBox(
+              height: 20.0,
+            ),
+            Padding(
+              padding: const EdgeInsets.all(8.0),
+              child: Card(
+                color: Colors.yellow,
+                child: Column(
+                  mainAxisSize: MainAxisSize.min,
+                  children: <Widget>[
+                    ListTile(
+                      onTap: () {
+                        //debugPrint("Clicked!");
+                      },
+                      leading: Icon(Icons.fastfood),
+                      title: Text("Cooking list"),
+                      subtitle:
+                          Text("Add/Remove recipes from your cooking list"),
+                    ),
+                  ],
                 ),
               ),
             ),
