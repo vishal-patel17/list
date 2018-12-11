c304015b959e11a346b3bec671a8e36eaaeeedd1
Patel <vp185104@ncr.com>
2018-12-11 15:30:40 +0530

    DB connection

--- lib/shoppingList.dart
+++ lib/shoppingList.dart
@@ -1,10 +1,14 @@
 import 'package:flutter/material.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:async/async.dart';
 
 class ShoppingList extends StatefulWidget {
   _ShoppingListState createState() => _ShoppingListState();
 }
 
 class _ShoppingListState extends State<ShoppingList> {
+  String item;
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
@@ -13,8 +17,72 @@ class _ShoppingListState extends State<ShoppingList> {
         appBar: AppBar(
           title: Text("Shopping List"),
         ),
-        body: ListView(),
+        floatingActionButton: FloatingActionButton(
+          onPressed: () {
+            showDialog(
+                context: context,
+                builder: (BuildContext context) {
+                  return AlertDialog(
+                    title: Text("Enter an item"),
+                    content: TextField(
+                      onChanged: (String value) {
+                        setState(() {
+                          this.item = value;
+                        });
+                      },
+                    ),
+                    actions: <Widget>[
+                      FlatButton(
+                        child: Icon(Icons.add),
+                        onPressed: () {
+                          Firestore.instance
+                              .runTransaction((Transaction transaction) async {
+                            CollectionReference reference =
+                                Firestore.instance.collection('shopping_list');
+                            await reference.add({"name": item, "done": "Pending"});
+                          });
+                          Navigator.pop(context);
+                        },
+                      )
+                    ],
+                  );
+                });
+          },
+          child: Icon(Icons.add_shopping_cart),
+        ),
+        body: List1(),
       ),
+    );
+  }
+}
+
+class List1 extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return StreamBuilder<QuerySnapshot>(
+      stream: Firestore.instance.collection('shopping_list').snapshots(),
+      builder: (BuildContext context, AsyncSnapshot<QuerySnapshot> snapshot) {
+        if (snapshot.hasError) return Text('Error: ${snapshot.error}');
+        switch (snapshot.connectionState) {
+          case ConnectionState.waiting:
+            return CircularProgressIndicator();
+          default:
+            return ListView(
+              children:
+                  snapshot.data.documents.map((DocumentSnapshot document) {
+                return Padding(
+                  padding: const EdgeInsets.all(8.0),
+                  child: Card(
+                    child: ListTile(
+                      title: Text(document['name']),
+                      subtitle: Text(document['done']),
+                    ),
+                  ),
+                );
+              }).toList(),
+            );
+        }
+      },
     );
   }
 }
